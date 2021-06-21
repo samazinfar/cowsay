7196472993eb8ed769a8a71e4f2fde0d3da9b831
samazinfar <46926694+samazinfar@users.noreply.github.com>
2021-06-21 11:58:36 +0200

    Create information.js

---
+++ information.js
@@ -0,0 +1,5 @@
+let presentation = {name: "Pauline", campus: "Wild Code School BXL"};
+const id =() => {
+        return `Hello, i'm ${presentation.name} from ${presentation.campus}`;
+};
+module.exports=id;
